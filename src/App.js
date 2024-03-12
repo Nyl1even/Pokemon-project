03f76fea1d5153f40f5fbe24b47f47c06f9fdc53
Nyl1even <lgarzino30@gmail.com>
2024-03-12 10:16:51 +0100

    commit 3

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, {useCallback} from 'react'
 import {useDropzone} from 'react-dropzone'
-
+import 'bootstrap/dist/css/bootstrap.min.css';
 function Dropzone() {
   const onDrop = useCallback(acceptedFiles => {
     // Do something with the files
@@ -25,20 +25,29 @@ function App() {
       <header>
         <div className="header">
           <img src={require('./pokemon-logo.png')} alt="pokemon logo" className='logo' />
-          <div className="navbar">
-            <div className="item-navbar">
-              <p href="https://www.pokemon.com/us/pokedex/" target="_blank">About</p>
+          <nav className="navbar navbar-expand-lg navbar-light bg-light">
+            <div className="container">
+              <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span className="navbar-toggler-icon"></span>
+              </button>
+              <div className="collapse navbar-collapse" id="navbarNav">
+                <ul className="navbar-nav">
+                  <li className="nav-item">
+                    <a className="nav-link" href="https://www.pokemon.com/us/pokedex/" target="_blank">About</a>
+                  </li>
+                  <li className="nav-item">
+                    <a className="nav-link" href="https://www.pokemon.com/us/pokedex/" target="_blank">Contact</a>
+                  </li>
+                  <li className="nav-item">
+                    <a className="nav-link" href="https://www.pokemon.com/us/pokedex/" target="_blank">Pokedex</a>
+                  </li>
+                  <li className="nav-item">
+                    <a className="nav-link" href="https://www.pokemon.com/us/pokedex/" target="_blank">Account</a>
+                  </li>
+                </ul>
+              </div>
             </div>
-            <div className="item-navbar">
-              <p href="https://www.pokemon.com/us/pokedex/" target="_blank">Contact</p>
-            </div>
-            <div className="item-navbar">
-              <p href="https://www.pokemon.com/us/pokedex/" target="_blank">Pokedex</p>
-            </div>
-            <div className="item-navbar">
-              <p href="https://www.pokemon.com/us/pokedex/" target="_blank">Account</p>
-            </div>
-          </div>
+          </nav>
         </div>
       </header>
       <div className="main-page">
